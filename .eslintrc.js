7c87a889a348242b43fd96b11e836d0b80cc5622
Graham Nessler <grahamnessler@insiderx.com>
2020-12-22 11:30:18 -0600

    Fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,13 @@
       ecmaFeatures: {
         jsx: true
       },
-
     },
     extends: [
       'eslint:recommended',
     ],
     rules: {
       'no-console': 'warn',
+      'no-unused-vars': 'warn',
     },
     env: {
       browser: true,

1b9d0f483afe9060720d00496ec165d7041f4e36
gness1804 <graham.nessler@gmail.com>
2020-12-23 15:50:18 -0600

    Update state on input entry

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+    };
+  }
+
   render() {
     return (
       <div>
         <h2>Create a New Song</h2>
-        <form></form>
+        <form>
+          <label htmlFor="song-entry">Song Title:</label>
+          <input
+            id="song-entry"
+            type="text"
+            onChange={(e) => this.setState({ title: e.target.value })}
+            value={this.state.title}
+          />
+        </form>
       </div>
     );
   }
